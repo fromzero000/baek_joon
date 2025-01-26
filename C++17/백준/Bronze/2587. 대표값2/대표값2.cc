ec9c2fbd92a083c11ccc593b9122695eea311feb
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2025-01-26 14:55:23 +0900

    [Bronze II] Title: 대표값2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2587. 대표값2/대표값2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a[5];
+    int sum = 0;
+    for(int i = 0; i < 5; i++){
+        cin>>a[i];
+        sum+=a[i];
+    }
+    sort(a,a+5);
+    cout<<sum/5<<'\n'<<a[2];
+    
+    
+}
