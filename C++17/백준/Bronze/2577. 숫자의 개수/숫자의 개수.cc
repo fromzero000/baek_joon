d713aa719d60ab4665268a1195a3e6dc194a11a2
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2025-02-01 16:22:59 +0900

    [Bronze II] Title: 숫자의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
+++ C++17/백준/Bronze/2577. 숫자의 개수/숫자의 개수.cc
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
-
+int ds[10];
 int main(){
     long a,b,c,d;
     cin>>a>>b>>c;
     d = a*b*c;
-    int ds[10];
-    for(int i = 0; i < 10; i++)
-        ds[i] = 0;
+
+
     while(d){
         int i = d%10;
         ds[i]++;
