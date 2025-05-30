a4fae2e4ca6a270dfc8a3be0df6562bfb964c2f6
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2025-05-30 23:29:12 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 2.85 ms, Memory: 4.21 MB -BaekjoonHub

--- C++/프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
+++ C++/프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -1,20 +1,18 @@
 #include <string>
 #include <vector>
 #include <cmath>
-
 using namespace std;
 
 long long solution(int r1, int r2) {
     long long answer = 0;
     
     for(long long i = 0; i <= r2; i++){
-        //r1^2 <= i^2 + x^2 <= r2^2 ==> r1 <=i^2+x^2
-        long long smallCircle;
-        long long bigCircle = floor(sqrt(pow(r2,2)-pow(i,2)));
-        if(i<=r1) smallCircle = ceil(sqrt(pow(r1,2)-pow(i,2)));
-        else smallCircle = 0;
-        answer += bigCircle - smallCircle + 1;
+        long long bc = sqrt(pow(r2,2)-pow(i,2));
+        long long sc = (i<=r1)?ceil(sqrt(pow(r1,2)-pow(i,2))):0;
+        answer += bc - sc + 1;
+        
     }
-    answer *= 4; answer -= (r2-r1+1)*4;
+    answer *= 4;
+    answer -= 4*(r2-r1+1);
     return answer;
 }
