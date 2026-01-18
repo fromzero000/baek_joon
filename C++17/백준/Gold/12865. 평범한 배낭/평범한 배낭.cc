d789534e8e919b17dd9af362340953e44ca556ea
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2026-01-18 14:09:02 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41476 KB -BaekjoonHub

--- C++17/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ C++17/백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -1,29 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int w[101];
+int v[101];
+int dp[101][100001];
 int n, k;
-int w[105];
-int v[105];
-//6 13
-//4 8
-//3 6
-//5 12
-int dp[105][1000005];
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cin>>n>>k;
-    for(int i = 1; i <= n; i++){
-        cin>>w[i]>>v[i];
-    }
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= k; j++){
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> k;
+    for(int i = 1; i <= n; i++)
+    {
+        cin >> w[i] >> v[i];
+    }
+    
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = 1; j <= k; j++)
+        {
             dp[i][j] = dp[i-1][j];
-            if(j-w[i]>=0){
-                dp[i][j] = max(dp[i][j],dp[i-1][j-w[i]]+v[i]);
-            }
+            if(j - w[i] >= 0)
+                dp[i][j] = max(dp[i][j], dp[i-1][j-w[i]] + v[i]);
         }
     }
-    cout<<dp[n][k];
+    
+    cout << dp[n][k];
 }
