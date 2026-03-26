3e3287627d6f439af42916f2c5c25128375d8398
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2026-03-26 23:12:06 +0900

    [Silver I] Title: 완전 이진 트리, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

--- C++17/백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
+++ C++17/백준/Silver/9934. 완전 이진 트리/완전 이진 트리.cc
@@ -31,9 +31,9 @@ int main()
     
     inorder(0, n-1, 0);
     
-    for(int i = 0; i < k; i++)
+    for(const vector<int>& level : tree)
     {
-        for(int node : tree[i])
+        for(const int& node : level)
             cout << node << ' ';
         cout << '\n';
     }
