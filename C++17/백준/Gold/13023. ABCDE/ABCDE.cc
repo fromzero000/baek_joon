a09a160aa9212ee605813896e1ba9e725b09cabb
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2026-03-26 18:34:50 +0900

    [Gold V] Title: ABCDE, Time: 72 ms, Memory: 2204 KB -BaekjoonHub

--- C++17/백준/Gold/13023. ABCDE/ABCDE.cc
+++ C++17/백준/Gold/13023. ABCDE/ABCDE.cc
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int n, m;
 vector<int> friends[2000];
-bool visited[2000];
 
-bool solve(int cur, int depth)
+bool solve(int cur, int depth, bitset<2000> visited)
 {
-    if(depth >= 5)
+    if(depth == 5)
         return true;
     
-    for(const int& next : friends[cur])
-    {
-        if(visited[next]) continue;
-        visited[next] = true;
-        if(solve(next, depth+1))
-        {
-            return true;
-        }
-        visited[next] = false;
-    }
-    visited[cur] = false;  
-    return false;
+    visited.set(cur);
+    return any_of(friends[cur].begin(), friends[cur].end(), [&](int next)
+                  {
+                      if(!visited[next])
+                      {
+                          return solve(next, depth+1, visited);
+                      }
+                      return false;
+                  });
+    
 }
 
 int main()
@@ -30,24 +25,22 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
+    int n, m;
     cin >> n >> m;
-    for(int i = 0; i < m; i++)
-    {
+    
+    for(int i = 0; i < m; i++) {
         int a, b;
         cin >> a >> b;
         friends[a].push_back(b);
         friends[b].push_back(a);
     }
+
+    vector<int> nodes(n);
+    iota(nodes.begin(), nodes.end(), 0);
     
-    for(int i = 0; i < n; i++)
-    {
-        fill(visited, visited+n, false);
-        visited[i] = 1;
-        if(solve(i, 1))
-        {
-            cout << 1;
-            return 0;
-        }
-    }
-    cout << 0;
+    bool result = any_of(nodes.begin(),nodes.end(), [](int start_node){return solve(start_node, 1, bitset<2000>());});
+        
+    if(result)
+        cout << 1;
+    else cout << 0;
 }
