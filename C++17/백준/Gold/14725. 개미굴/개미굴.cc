702fe44e973bd39df1d7ce7d69c7f7ee55572cef
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2026-03-28 16:08:14 +0900

    [Gold III] Title: 개미굴, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- C++17/백준/Gold/14725. 개미굴/개미굴.cc
+++ C++17/백준/Gold/14725. 개미굴/개미굴.cc
@@ -37,7 +37,7 @@ int main()
         for(int j = 0; j < k; j++)
         {
             cin >> next;
-            cur = &(cur->child[next]);
+            cur = &cur->child[next];
         }
     }
     
