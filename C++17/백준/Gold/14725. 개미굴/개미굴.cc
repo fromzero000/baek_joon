c4bbd9174dfdb9af24bd87d75631dea64ee77517
fromzero000 <168260621+fromzero000@users.noreply.github.com>
2026-03-28 16:06:51 +0900

    [Gold III] Title: 개미굴, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

---
+++ C++17/백준/Gold/14725. 개미굴/개미굴.cc
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Cave
+{
+    map<string, Cave> child{};
+};
+
+
+void tree_r(const Cave& root, int depth)
+{
+    for(const auto& p : root.child)
+    {
+        for(int d = 0; d < depth; d++) cout << "--";
+        cout << p.first << '\n';
+        tree_r(p.second, depth+1);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n;
+    
+    cin >> n;
+    
+    Cave root{};
+    for(int i = 0; i < n; i++)
+    {
+        int k;
+        cin >> k;
+        Cave* cur = &root;
+        string next;
+        
+        for(int j = 0; j < k; j++)
+        {
+            cin >> next;
+            if(cur->child.find(next)==cur->child.end())
+                cur->child.insert({next,{}});
+            cur = &cur->child[next];
+        }
+    }
+    
+    tree_r(root, 0);
+}
